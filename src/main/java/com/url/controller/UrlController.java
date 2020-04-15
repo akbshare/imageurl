199c8bdd748d00150f900b475b4498c95697f7ed
Akbar Shareef <akbar.shareef@trivium-esolutions.com>
2020-04-16 00:38:01 +0530

    passing image name as path variable

--- src/main/java/com/url/controller/UrlController.java
+++ src/main/java/com/url/controller/UrlController.java
@@ -4,6 +4,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,13 @@ import java.io.IOException;
 
 @RestController
 public class UrlController {
-    @RequestMapping(value = "/share", method = RequestMethod.GET,
+    @RequestMapping(value = "/share/{image}", method = RequestMethod.GET,
             produces = MediaType.IMAGE_PNG_VALUE)
             //produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> getImage() throws IOException {
+    public ResponseEntity<byte[]> getImage(@PathVariable String image) throws IOException {
         //var imgFile = new ClassPathResource("images/corona-two.jpeg");
-        ClassPathResource imgFile = new ClassPathResource("images/image-one.png");
+        //ClassPathResource imgFile = new ClassPathResource("images/image-one.png");
+        ClassPathResource imgFile = new ClassPathResource("images/"+image);
         byte[] bytes = StreamUtils.copyToByteArray(imgFile.getInputStream());
 
         return ResponseEntity
